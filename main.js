a00a99dbe2954dbfdc7a0c6c08068d729baa240d
artrem3W <56219809+artremRemen@users.noreply.github.com>
2022-01-19 15:01:07 +0100

    2

--- main.js
+++ main.js
@@ -8,30 +8,41 @@
             console.log(fonts);
             this.FontsList = document.querySelectorAll('option');
             this.AppItem = document.querySelector('.app');
-            this.scoreDisplay = document.querySelector("information__score");
-            this.timeDisplay = document.querySelector("information__time");
+            this.scoreDisplay = document.querySelector(".information__score");
+            this.timeDisplay = document.querySelector(".information__time");
+            this.submitButton = document.querySelector('[value="Valider"]');
             this.score = 0;
-            this.time = 20;
+            this.time = 0;
 
-            this.AppItem.innerHTML = `<li data-font-name="${fonts[1].name}" data-family="${fonts[1].familly}" class='app__item'>
+            /* Functions */
+            const RandomNumber = () => {
+                this.number = parseInt(Math.random(0, 20) * 10 * 2);
+                return this.number;
+            }
+
+
+
+            this.IntervalId = setInterval((e) => {
+                console.log('test');
+                this.time++
+
+                    this.scoreDisplay.innerText = `Temps restant pour cette carte:00:${this.time}.`;
+
+            }, 1000);
+
+            if (this.time === 20) {
+
+                clearInterval(this.IntervalId);
+            }
+
+            this.AppItem.innerHTML = `<li data-font-name="${fonts[RandomNumber()].name}" data-family="${fonts[RandomNumber()].familly}" class='app__item'>
             <div class="app__item__info"><span class="app__item__info__name">nom</span>
               <span class="app__item__info__info">famille - auteur</span>
             </div>
-            <img class='app__item__font' src='./assets/fonts/${fonts[1].file}.svg' alt='Aa, abcdefghijklmnopqrstuvwxyz, ABCDEFGHIJKLMNOPQRSTUVWXYZ'>
-          </li>`;
+            <img class='app__item__font' src='./assets/fonts/${fonts[RandomNumber()].file}.svg' alt='Aa, abcdefghijklmnopqrstuvwxyz, ABCDEFGHIJKLMNOPQRSTUVWXYZ'>
+            </li>`;
 
         }
     }
     Tymper.init();
 })();
-
-
-
-{
-    /* <li data-font-name="nom" data-family="famille" class='app__item'>
-      <div class="app__item__info"><span class="app__item__info__name">nom</span>
-        <span class="app__item__info__info">famille - auteur</span>
-      </div>
-      <img class='app__item__font' src='./assets/fonts/nom.svg' alt='Aa, abcdefghijklmnopqrstuvwxyz, ABCDEFGHIJKLMNOPQRSTUVWXYZ'>
-    </li> */
-}
